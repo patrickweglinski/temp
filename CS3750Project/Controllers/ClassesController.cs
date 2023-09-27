11dddc8d98edfc5fa3d5e6cdb47ff3b86aed6d47
ethanharden <ethanjharden3@gmail.com>
2023-09-26 18:39:40 -0600

    Class not working

---
+++ CS3750Project/Controllers/ClassesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CS3750Project.Data;
+using CS3750Project.Models;
+
+namespace CS3750Project.Controllers
+{
+    public class ClassesController : Controller
+    {
+        private readonly CS3750ProjectContext _context;
+
+        public ClassesController(CS3750ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Classes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Class != null ? 
+                          View(await _context.Class.ToListAsync()) :
+                          Problem("Entity set 'CS3750ProjectContext.Class'  is null.");
+        }
+
+        // GET: Classes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Class == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // GET: Classes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Classes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ClassDept,ClassName,CreditHours,Location,DaysOfWeek,TimeOfDay")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@class);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@class);
+        }
+
+        // GET: Classes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Class == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+            return View(@class);
+        }
+
+        // POST: Classes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ClassDept,ClassName,CreditHours,Location,DaysOfWeek,TimeOfDay")] Class @class)
+        {
+            if (id != @class.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@class);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassExists(@class.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@class);
+        }
+
+        // GET: Classes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Class == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Class
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // POST: Classes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Class == null)
+            {
+                return Problem("Entity set 'CS3750ProjectContext.Class'  is null.");
+            }
+            var @class = await _context.Class.FindAsync(id);
+            if (@class != null)
+            {
+                _context.Class.Remove(@class);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassExists(int id)
+        {
+          return (_context.Class?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CS3750Project/Models/Class.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CS3750Project.Models
+{
+    public class Class
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String ClassDept { get; set; }
+        [Required]
+        public String ClassName { get; set; }
+        [Required]
+        public int CreditHours { get; set; }
+        [Required]
+        public String Location { get; set; }
+        [Required]
+        public String DaysOfWeek { get; set; }
+        [Required]
+        public String TimeOfDay {  get; set; }
+        
+     
+        
+    }
+}

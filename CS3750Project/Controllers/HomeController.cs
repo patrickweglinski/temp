e27f7e3e39613d41afbff6eb22758e98082b48b9
Nicholas <17nfrederick@gmail.com>
2023-10-02 19:12:09 -0600

    Update HomeController.cs

--- CS3750Project/Controllers/HomeController.cs
+++ CS3750Project/Controllers/HomeController.cs
@@ -6,9 +6,44 @@ namespace CS3750Project.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly CS3750ProjectContext _context;
+
+        public HomeController(CS3750ProjectContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Class> classes = GetClassesFromDatabase();
+
+            return View(classes);
+        }
+
+        private List<Class> GetClassesFromDatabase()
+        {
+            var classes = _context.Class
+                .Select(c => new Class
+                {
+                    ClassName = c.ClassName,
+                    ClassDept = c.ClassDept,
+                    ClassNumber = c.ClassNumber,
+                    Location = c.Location,
+                    StartTime = c.StartTime,
+                    Monday = c.Monday,
+                    Tuesday = c.Tuesday,
+                    Wednesday = c.Wednesday,
+                    Thursday = c.Thursday,
+                    Friday = c.Friday,
+                    Saturday = c.Saturday,
+                    Sunday = c.Sunday,
+                    EndTime = c.EndTime
+                })
+                .ToList();
+
+            // var classes2 = _context.Class.Where(x => x.InstructorId = 32).ToList();
+
+            return classes;
         }
 
         public IActionResult Privacy()
@@ -21,7 +56,11 @@ namespace CS3750Project.Controllers
             string id = HttpContext.Session.GetString("GetUser");
             return RedirectToAction("Index", "Profile", new { email = id });
         }
-
+        public IActionResult Classes()
+        {
+            string id = HttpContext.Session.GetString("GetUser");
+            return RedirectToAction("Index", "Classes", new { email = id });
+        }
         public IActionResult Upload()
         {
             return RedirectToAction("Index", "Upload");
@@ -32,10 +71,10 @@ namespace CS3750Project.Controllers
             string id = HttpContext.Session.GetString("GetUser");
             return RedirectToAction("Index", "Registration", new { email = id });
         }
-
-        public IActionResult Classes()
+        
+        public IActionResult Calendar()
         {
-            return RedirectToAction("Index", "Classes");
+            return RedirectToAction("Index", "Calendar");
         }
 
     }
